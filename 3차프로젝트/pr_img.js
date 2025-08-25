1efc3402045e956ffc06eb299c6fc8f27f909d41
Hongdroid <79145393+Hongdroid@users.noreply.github.com>
2025-08-25 21:07:00 +0900

    Delete 3차프로젝트 directory

--- 3차프로젝트/pr_img.js
+++
@@ -1,9 +0,0 @@
-function setThumbnail(event) { 
-    var reader = new FileReader(); 
-    reader.onload = function(event) { 
-        var img = document.createElement("img"); 
-        img.setAttribute("src", event.target.result); 
-        document.querySelector("div#img_container").appendChild(img); 
-    }; 
-        reader.readAsDataURL(event.target.files[0]); 
-    }
